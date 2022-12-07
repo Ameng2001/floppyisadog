0941c65f2793931859898c63b154d52a3724e77d
Ameng2001 <ameng2001.liu@gmail.com>
2022-12-07 20:51:21 +0800

    frontend added

---
+++ frontend/myaccount/src/components/CheckboxField/index.js
@@ -0,0 +1,29 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+
+require('./checkbox-field.scss');
+
+function CheckboxField({ id, description, attribute, checked, onChange }) {
+  return (
+    <div className="checkbox-field">
+      <input
+        name={id}
+        id={id}
+        type="checkbox"
+        data-model-attribute={attribute}
+        checked={checked}
+        onChange={onChange}
+      />
+      <span>{description}</span>
+    </div>
+  );
+}
+CheckboxField.propTypes = {
+  attribute: PropTypes.string,
+  checked: React.PropTypes.bool,
+  description: PropTypes.string.isRequired,
+  id: PropTypes.string.isRequired,
+  onChange: React.PropTypes.func,
+};
+
+export default CheckboxField;
